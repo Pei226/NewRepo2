06da66201090a3d5a4306fa8c54ab4e837845bda
student <student@E60760>
2024-12-05 11:18:26 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1112410043_1205
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float score = 0;
+            Console.WriteLine("請輸入成績:");
+            try
+            {
+                score = float.Parse(Console.ReadLine());
+            }
+            catch 
+            { 
+            Console.WriteLine("輸入資料錯誤");
+            return;
+            }
+            Console.WriteLine("你輸入的分數為:"+score.ToString());
+        }
+    }
+}
